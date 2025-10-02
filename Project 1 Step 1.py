df6783a8bc244cd2986a03d25287d4c20ef3f42a
ArmanS18 <a1suleman@torontomu.ca>
2025-10-02 12:08:46 -0400

    Step 1
    Import, read, and analyze data

---
+++ Project 1 Step 1.py
@@ -0,0 +1,14 @@
+#Step 1
+import pandas as pd
+
+df = pd.read_csv("Project 1 Data.csv")
+
+print("Dataset shape:", df.shape)
+print("First 5 rows:")
+print(df.head())
+
+print("\nColumn info:")
+print(df.info())
+
+print("\nMissing values:")
+print(df.isnull().sum())
