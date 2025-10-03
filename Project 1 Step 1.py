1bb9a5f2e4902fcc24096e1b0b09ff1cc66bf44c
ArmanS18 <a1suleman@torontomu.ca>
2025-10-03 18:03:58 -0400

    Step 2 complete

--- Project 1 Step 1.py
+++ Project 1 Step 1.py
@@ -1,5 +1,9 @@
-#Step 1
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+#Step 1
 
 file_path = r'C:\Users\arman\OneDrive\Documents\GitHub\AER850_Project1\Project 1 Data.csv'
 df = pd.read_csv(file_path)
@@ -13,3 +17,39 @@ print(df.info())
 
 print("\nMissing values:")
 print(df.isnull().sum())
+
+#Step 2
+
+#Data Inspection
+print ("Summary statistics:")
+print(df.describe())
+
+features = ['X', 'Y', 'Z']
+target = 'Step'
+
+#Histogram
+df[features].hist(figsize=(10, 6), bins=20)
+plt.suptitle("Fetaure Distribution", fontsize=14)
+plt.show()
+
+#Boxplots
+plt.figure(figsize=(12, 6))
+for i, col in enumerate(features, 1):
+    plt.subplot(1, 3, i)
+    sns.boxplot(x=target, y=col, data=df)
+    plt.title(f"{col} by {target}")
+plt.tight_layout()
+plt.show()
+
+#Pairplot
+sns.pairplot(df, hue=target, vars=features, palette="Set2")
+plt.show()
+
+#Class Balance
+
+plt.figure(figsize=(6, 4))
+df[target].value_counts().plot(kind='bar')
+plt.title("Class Distribution")
+plt.xlabel("Step")
+plt.ylabel("Count")
+plt.show()
